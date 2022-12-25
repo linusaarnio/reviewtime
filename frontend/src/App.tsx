0b2b31a3ea2e0ef9bdf28a0743268a5b82f9e31d
Linus Aarnio <linus.aarnio@stabelo.se>
2022-12-25 19:43:08 +0100

    WIP reviewing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage, { homeLoader } from "./routes/home";
+import ReviewingPage, { reviewingLoader } from "./routes/reviewing";
 import Root, { rootLoader } from "./routes/root";
 
 const router = createBrowserRouter([
@@ -15,6 +16,11 @@ const router = createBrowserRouter([
         path: "/",
         element: <HomePage />,
         loader: homeLoader,
+      },
+      {
+        path: "/reviewing",
+        element: <ReviewingPage />,
+        loader: reviewingLoader,
       }
     ]
   },
--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -35,10 +35,7 @@ interface Data {
 }
 
 export const rootLoader: () => Promise<Data> = async () => {
-  return {
-    profile: { name: "Linus Aarnio", avatarUrl: "https://avatars.githubusercontent.com/u/42450444?v=4" },
-    organization: "LiU",
-  };
+  return { profile: { name: "Linus Aarnio", avatarUrl: "https://avatars.githubusercontent.com/u/42450444?v=4" }, organization: "LiU" };
 };
 
 export default function PageRoot() {
@@ -47,6 +44,7 @@ export default function PageRoot() {
 
   return (
     <>
+
       <div>
         <Transition.Root show={sidebarOpen} as={Fragment}>
           <Dialog as="div" className="relative z-40 md:hidden" onClose={setSidebarOpen}>
@@ -210,26 +208,13 @@ export default function PageRoot() {
               <div className="px-4 sm:px-6 lg:mx-auto lg:max-w-6xl lg:px-8">
                 <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
                   <div className="min-w-0 flex-1">
-                    {/* Profile */}
                     <div className="flex items-center">
-                      <img className="hidden h-16 w-16 rounded-full sm:block" src={data.profile.avatarUrl} alt="" />
                       <div>
                         <div className="flex items-center">
-                          <img className="h-16 w-16 rounded-full sm:hidden" src={data.profile.avatarUrl} alt="" />
                           <h1 className="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:leading-9">
-                            It's Reviewtime, {data.profile.name}!
+                            It's Reviewtime!
                           </h1>
                         </div>
-                        <dl className="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
-                          <dt className="sr-only">Company</dt>
-                          <dd className="flex items-center text-sm font-medium capitalize text-gray-500 sm:mr-6">
-                            <BuildingOfficeIcon
-                              className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                              aria-hidden="true"
-                            />
-                            {data.organization}
-                          </dd>
-                        </dl>
                       </div>
                     </div>
                   </div>
